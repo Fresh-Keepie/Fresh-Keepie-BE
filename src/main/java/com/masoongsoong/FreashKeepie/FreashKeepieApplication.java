72652f8a9806ac825154f08424327bfb65954d0f
smin7 <stacycho0728@naver.com>
2024-04-09 13:04:17 +0900

    레layout

---
+++ src/main/java/com/masoongsoong/FreashKeepie/FreashKeepieApplication.java
@@ -0,0 +1,13 @@
+package com.masoongsoong.FreashKeepie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreashKeepieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreashKeepieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masoongsoong/FreashKeepie/FreashKeepieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masoongsoong.FreashKeepie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreashKeepieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
